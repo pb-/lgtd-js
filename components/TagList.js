d2b05f382f35791d30df3dd0a1e172fe7cedef48
Paul Baecher <pbaecher@gmail.com>
2016-02-19 19:53:15 +0100

    start implementing item scheduling (dnd on tickler)

--- components/TagList.js
+++ components/TagList.js
@@ -1,12 +1,44 @@
 import React, { PropTypes, Component } from 'react'
 import ReactDOM from 'react-dom'
+import moment from 'moment'
+
+
+function monthDays(firstDay) {
+  let days = []
+
+  for (let d = firstDay.clone(); d.month() == firstDay.month(); d.add(1, 'days')) {
+    days.push(d.clone())
+  }
+
+  return days
+}
+
+
+function weekChunks(days) {
+  let weeks = []
+  const first = days[0].weekday()
+  const last = days[days.length - 1].weekday()
+
+  // pad to full weeks
+  let padded = Array.concat(
+    Array(first).fill(null), days, Array(6 - last).fill(null))
+
+  // chunk into sizes of 7
+  while (padded.length > 0) {
+    weeks.push(padded.splice(0, 7))
+  }
+
+  return weeks
+}
+
 
 export default class TagList extends Component {
   constructor(props) {
     super(props)
     this.onDropItem = this.onDropItem.bind(this)
     this.state = {
-      addItemId: null
+      addItemId: null,
+      scheduleItemId: null,
     }
   }
 
@@ -19,6 +51,14 @@ export default class TagList extends Component {
   onDropItem(e, tag) {
     e.preventDefault()
     this.props.onSetTag(e.dataTransfer.getData('itemId'), tag)
+  }
+
+  onDropItemTickler(e) {
+    e.preventDefault()
+    this.setState({
+      scheduleItemId: e.dataTransfer.getData('itemId'),
+      selectedMonth: moment()
+    })
   }
 
   onAddItem(e) {
@@ -55,6 +95,24 @@ export default class TagList extends Component {
     this.props.onDeleteTag(tag)
   }
 
+  onChangeMonth(e, change) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    this.setState({
+      selectedMonth: this.state.selectedMonth.clone().add(change, 'months')
+    })
+  }
+
+  onSchedule(e, day) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    const tag = '$' + day.format('YYYY-MM-DD')
+    this.props.onSetTag(this.state.scheduleItemId, tag)
+    this.setState({scheduleItemId: null})
+  }
+
   renderDeleteLink(tag) {
     if (tag.count === 0) {
       return <a className="remove" onClick={e => this.onDeleteTag(e, tag.name)}>❌</a>
@@ -67,15 +125,46 @@ export default class TagList extends Component {
     }
   }
 
+  renderTicklerCalendar() {
+    const month = this.state.selectedMonth.startOf('month')
+    const weeks = weekChunks(monthDays(month))
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="5">{month.format('MMMM YYYY')}</th>
+            <th onClick={e => this.onChangeMonth(e, -1)}>◂</th>
+            <th onClick={e => this.onChangeMonth(e, +1)}>▸</th>
+          </tr>
+          <tr>
+            {moment.weekdaysMin().map(day => <th key={day}>{day}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {weeks.map((week, i) =>
+            <tr key={i}>
+              {week.map((day, j) =>
+                <td key={j} onClick={e => this.onSchedule(e, day)}>
+                  {day === null ? '' : day.date()}
+                </td>)}
+            </tr>)}
+        </tbody>
+      </table>
+    )
+  }
+
   renderTag(tag) {
     const { name, count } = tag
     return (
       <li key={name} className={name === this.props.activeTag ? 'active' : ''}
           onClick={e => this.props.onSwitchTag(e, name)}
           onDragOver={e => e.preventDefault()}
-          onDrop={e => this.onDropItem(e, name)} >
+          onDrop={e => name ==='tickler' ? this.onDropItemTickler(e) : this.onDropItem(e, name)} >
         {name}{this.renderCount(count)}
         {this.renderDeleteLink(tag)}
+        {name === 'tickler' &&
+          this.state.scheduleItemId !== null ? this.renderTicklerCalendar() : ''}
       </li>
     )
   }
