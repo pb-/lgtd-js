62b26ddc0b5b7b39c5a7cdfbdb04d7f530d2c41a
Paul Baecher <pbaecher@gmail.com>
2016-02-27 21:53:53 +0100

    fix up package.json and add deploy script

---
+++ production.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: 'static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+}
